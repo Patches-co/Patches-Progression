939e9bb4fe392477994851331fc0baa61214d52d
Pacheco <jvvpacheco@gmail.com>
2026-05-27 13:58:16 -0400

    Flint tools -> Stone Tools; Copper tools are now a new item; knapped_rock added.

---
+++ src/main/java/net/patches/progression/item/ModToolMaterials.java
@@ -0,0 +1,67 @@
+package net.patches.progression.item;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Items;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.registry.tag.TagKey;
+import java.util.function.Supplier;
+
+public enum ModToolMaterials implements ToolMaterial {
+
+    COPPER(
+            BlockTags.INCORRECT_FOR_GOLD_TOOL,
+            131,
+            4.0F,
+            1.0F,
+            5,
+            () -> Ingredient.ofItems(Items.COPPER_INGOT)
+    );
+
+    private final TagKey<Block> inverseTag;
+    private final int itemDurability;
+    private final float miningSpeed;
+    private final float attackDamage;
+    private final int enchantability;
+    private final Supplier<Ingredient> repairIngredient;
+
+    ModToolMaterials(final TagKey<Block> inverseTag, final int itemDurability, final float miningSpeed, final float attackDamage, final int enchantability, final Supplier<Ingredient> repairIngredient) {
+        this.inverseTag = inverseTag;
+        this.itemDurability = itemDurability;
+        this.miningSpeed = miningSpeed;
+        this.attackDamage = attackDamage;
+        this.enchantability = enchantability;
+        this.repairIngredient = repairIngredient;
+    }
+
+    @Override
+    public int getDurability() {
+        return this.itemDurability;
+    }
+
+    @Override
+    public float getMiningSpeedMultiplier() {
+        return this.miningSpeed;
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return this.attackDamage;
+    }
+
+    @Override
+    public TagKey<Block> getInverseTag() {
+        return this.inverseTag;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return this.enchantability;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return this.repairIngredient.get();
+    }
+}
